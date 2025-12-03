261944bbc9cae7f39ce7ae952c02c96daf9a94b7
akashverma92 <akashverma.ara23@gmail.com>
2025-12-03 23:42:17 +0530

    first commit

---
+++ backend/src/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Default Route
+app.get("/", (req, res) => {
+  res.send("Voice Enabled Task Tracker Backend Running...");
+});
+
+// Server Start
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
